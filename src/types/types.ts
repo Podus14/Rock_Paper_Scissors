b5f399fd76a8bc1a09775a1551a1f90e0ef32332
KaRtUzYk <podus.leha@gmail.com>
2024-12-18 20:18:01 +0200

    Fixed comments last

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,7 +3,6 @@ export type Choice = "Paper" | "Rock" | "Scissors";
 
 export type Outcome = "You Win!" | "You Lose!" | "It's a Tie!"
 
-
 export type Score = {
     playerChoice: Choice,
     computerChoice: Choice, 
